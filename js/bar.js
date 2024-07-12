fc068e62ed6bbefbce1fb762e9083eeca15f7196
Victor Madruga Fernandes <victormafer@hotmail.com>
2024-07-12 20:42:07 -0300

    preload css

--- js/bar.js
+++ js/bar.js
@@ -1,20 +1,10 @@
-  // Seleciona a div #bar
-  var container = document.getElementById("bar");
-
-  // Array com os textos a serem repetidos
-  var textos = [" inspire ", " conquer ", " influence "];
-  
-  // Loop para criar e adicionar o conteúdo repetido
-  for(var j = 0; j < 10; j++){
+  var container = document.getElementById("bar"); // Seleciona a div #bar
+  var textos = [" inspire ", " conquer ", " influence "]; // Array com os textos a serem repetidos
+  for(var j = 0; j < 10; j++){ // Loop para criar e adicionar o conteúdo repetido
     for (var i = 0; i < textos.length; i++) {
-    // Cria um novo span com a classe "icon"
-    var spanIcon = document.createElement("span");
+    var spanIcon = document.createElement("span"); // Cria um novo span com a classe "icon"
     spanIcon.className = "icon";
-    
-    // Adiciona o span com a classe "icon" à div bar
-    container.appendChild(spanIcon);
-  
-    // Adiciona o texto atual à div bar
-    container.appendChild(document.createTextNode(textos[i]));
+    container.appendChild(spanIcon); // Adiciona o span com a classe "icon" à div bar
+    container.appendChild(document.createTextNode(textos[i])); // Adiciona o texto atual à div bar
     }
   }
--- js/buttons.js
+++ js/buttons.js
@@ -1,12 +1,12 @@
 function scrollToSection(id) {
-    var element = document.getElementById(id);
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
-      var headerHeight = document.querySelector('header').offsetHeight;
-      window.scrollBy(0, -headerHeight);
-    }
+  var element = document.getElementById(id);
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+    var headerHeight = document.querySelector('header').offsetHeight;
+    window.scrollBy(0, -headerHeight);
+  }
 }
   
 function redirectTo(url) {
-    window.location.href = url;
+  window.location.href = url;
 }
